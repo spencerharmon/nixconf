a63ce8714c57f304f691ca81411fd6a26679f2b0
Spencer Harmon <spencerharmon.91@gmail.com>
2022-07-08 01:30:08 -0500

    xhost

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -4,13 +4,21 @@
 
   services.xserver.enable = true;
   services.xserver.windowManager.exwm.enable = true;
+  services.xserver.windowManager.session = lib.singleton {
+    name = "exwm";
+    start = ''
+      xhost +SI:localuser:$USER
+      exec emacs
+    '';
+  };
   services.xserver.displayManager.lightdm.enable  = true;
   services.xserver.displayManager.lightdm.greeters.tiny.enable = true;
   services.xserver.displayManager.defaultSession = "none+exwm";
   environment = {
     systemPackages = with pkgs; [
-      firefox
+      firefox-bin
       emacs
+      pkgs.xorg.xhost
     ];
   };
 }
