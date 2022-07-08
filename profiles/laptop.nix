30fc9e3533c8490285366576ebf08b9ebf07a1dc
Spencer Harmon <spencerharmon.91@gmail.com>
2022-07-08 00:23:07 -0500

    semicolon

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -5,7 +5,7 @@
   services.xserver.enable = true;
   services.xserver.windowManager.exwm.enable = true;
   services.xserver.displayManager.lightdm.enable  = true;
-  services.xserver.displayManager.lightdm.greeters.tiny.enable = true
+  services.xserver.displayManager.lightdm.greeters.tiny.enable = true;
   environment = {
     systemPackages = with pkgs; [
       firefox
